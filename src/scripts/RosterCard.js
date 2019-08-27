8452436b05a199d383ad6c6e651692ebb2248d8b
angad <angad88@hotmail.com>
2019-08-27 19:25:47 -0400

    Remove fixed position for background
    Made sure that the all details were displayed instead of getting cut off

--- src/scripts/RosterCard.js
+++ src/scripts/RosterCard.js
@@ -3,35 +3,37 @@ import ReactImageAppear from 'react-image-appear';
 
 
 class RosterCard extends Component {
-    /**
-     * Represents the player card used in the Roster page. 
-     * 
-     * @param {*} props 
-     *    src - The src of the player image 
-     *    player - The player details 
-     */
+  /**
+   * Represents the player card used in the Roster page. 
+   * 
+   * @param {*} props 
+   *    src - The src of the player image 
+   *    player - The player details 
+   */
 
-    constructor(props) {
-      super(props);
-      this.state = {
-        isLoading: true
-      };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true
+    };
+  }
 
 
-    render() {
-      return (
-        <div className="card-body" style={{borderStyle: "solid", 
-          borderWidth: "2px", borderColor: "black"}}>
-          <ReactImageAppear src={this.props.src} 
-            animation="fadeIn" animationDuration="0.1s" showLoader={false}/>
-          <p style={{color: "black", textAlign: "center"}}
-            className="card-text">
-            {this.props.player.name}
-          </p>
-        </div>
-      );
-    }
+  render() {
+    return (
+      <div className="card-body" style={{
+        borderStyle: "solid",
+        borderWidth: "2px", borderColor: "black"
+      }}>
+        <ReactImageAppear src={this.props.src}
+          animation="fadeIn" animationDuration="0.1s" showLoader={false} />
+        <p style={{ color: "black", textAlign: "center" }}
+          className="card-text">
+          {this.props.player.name}
+        </p>
+      </div>
+    );
+  }
 }
 
 export default RosterCard;
